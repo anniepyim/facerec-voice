ada3df25644c31db9052a9cf4a01cd9b912d66d8
Annie Yim <anniepyim@gmail.com>
2020-06-07 13:29:51 +0200

    [FIX] MM port number

--- modules/mirror_handler.py
+++ modules/mirror_handler.py
@@ -8,7 +8,7 @@ import logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(name)s %(message)s')
 
-port = '8123'
+port_mirror = '8083'
 port_server = '8082'
 
 def keep_token(t):
@@ -18,7 +18,7 @@ def keep_token(t):
 def mirror_call(notification, reply):
     payload = {'notification': notification, 'reply': reply}
     params = urllib.parse.urlencode(payload, quote_via=urllib.parse.quote)
-    r = requests.get('http://localhost:'+port+'/ga?', params=params)
+    r = requests.get('http://localhost:'+port_mirror+'/ga?', params=params)
 
     logger.info(r.text)
 
@@ -26,7 +26,7 @@ def mirror_greet(persons):
 
     payload = {'type': 'INFO', 'message': persons, 'silent': 'true'}
     params = urllib.parse.urlencode(payload, quote_via=urllib.parse.quote)
-    r = requests.get('http://localhost:'+port+'/greetings?', params=params)
+    r = requests.get('http://localhost:'+port_mirror+'/greetings?', params=params)
 
     logger.info(r.text)
 
@@ -34,7 +34,7 @@ def mirror_youtube(load, url=""):
 
     payload = {'load': load, 'embed_url': url}
     params = urllib.parse.urlencode(payload, quote_via=urllib.parse.quote)
-    r = requests.get('http://localhost:'+port+'/youtube?', params=params)
+    r = requests.get('http://localhost:'+port_mirror+'/youtube?', params=params)
 
     logger.info(r.text)
 
@@ -59,13 +59,13 @@ def mirror_spotify(user_response, send_status=False):
             payload = {'notification': 'PLAY'}
         r = requests.get('http://localhost:' + port_server + '/spotify?spotify_changed=' + str(send_status))
         params = urllib.parse.urlencode(payload, quote_via=urllib.parse.quote)
-        r = requests.get('http://localhost:'+port+'/spotify?', params=params)
+        r = requests.get('http://localhost:'+port_mirror+'/spotify?', params=params)
         logger.info(r.text)
         return True
     else:
         return False
 
 def mirror_spotify_status():
-    r = requests.get('http://localhost:8123/spotify?', params='notification=GET_STATUS')
+    r = requests.get('http://localhost:' + port_mirror + '/spotify?', params='notification=GET_STATUS')
     status = json.loads(r.text)['is_playing'] == "true"
     return status
