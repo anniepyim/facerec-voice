e24ed176a42e98b6e926427ddb8e6c0a3ec1ee18
Annie Yim <anniepyim@gmail.com>
2020-06-07 16:18:49 +0200

    [FIX] killer script

--- kill.sh
+++ kill.sh
@@ -4,3 +4,7 @@ kill $(ps aux | grep 'python' | awk '{print $2}')
 kill $(ps aux | grep 'npm' | awk '{print $2}')
 kill $(ps aux | grep 'startup' | awk '{print $2}')
 kill $(ps aux | grep 'ngrok' | awk '{print $2}')
+
+kill $(sudo netstat -ap | grep :8081 | awk '{print $7}' | awk -F / '{print $1}')
+kill $(sudo netstat -ap | grep :8082 | awk '{print $7}' | awk -F / '{print $1}')
+kill $(sudo netstat -ap | grep :8083 | awk '{print $7}' | awk -F / '{print $1}')
